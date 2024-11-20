78ff0117e8b46422094d194a45152e71053d4db0
Cemal <c.igrek63@hotmail.com>
2024-11-20 15:45:49 +0300

    Im Learning Bean Scopes

--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/luve2code/learningSpring/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/luve2code/learningSpring/springcoredemo/rest/DemoController.java
@@ -12,17 +12,27 @@ public class DemoController {
     // @Autowired this one is not recommended by spring.io team because it makes harder to unit tests.
     private Coach myCoach;
 
+    private Coach anotherCoach;
+
 
     @Autowired
-    public DemoController (@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController (@Qualifier("cricketCoach") Coach theCoach,
+    @Qualifier("cricketCoach") Coach theAnotherCoach){
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String dailyWorkout(){
         return myCoach.getDailyWorkout();
     }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach ,"+ (myCoach == anotherCoach);
+    }
 }
+
 
 
 
