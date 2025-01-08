e72ba75c5f9f5f78d0ac138724a38ee679c163c2
Cemal <c.igrek63@hotmail.com>
2025-01-08 13:22:25 +0300

    I added a data to Mysql from Java IDE with Entity Manager.

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,10 +14,21 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 	return runner -> {
-		System.out.println("Hello World" );
+		createStudent(studentDAO);
 	};
 	}
 
+	private void createStudent(StudentDAO studentDAO) {
+		//create the student object
+		System.out.println("Creating new student...");
+		Student tempStudent = new Student("c.igrek63@hotmail.com","igrek","Cemal");
+
+		//save the student object
+		System.out.println("Saving student...");
+		studentDAO.save(tempStudent);
+		//display id of the save student
+		System.out.println("Student saved and his id is " + tempStudent.getId());
+	}
 }
