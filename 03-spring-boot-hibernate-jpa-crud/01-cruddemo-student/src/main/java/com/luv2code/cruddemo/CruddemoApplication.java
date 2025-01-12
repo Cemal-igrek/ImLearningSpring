8ff5b58c17ab44e3e17d1c9555729609a451c158
Cemal <c.igrek63@hotmail.com>
2025-01-13 01:32:39 +0300

    Deleting Objects with JPA

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -24,8 +24,22 @@ public class CruddemoApplication {
 //		readStudent(studentDAO);
 //		queryForStudents(studentDAO);
 //      queryForStudentsByLastName(studentDAO);
-        updateStudent(studentDAO);
+//        updateStudent(studentDAO);
+//        deleteStudent(studentDAO);
+          deleteAllStudents(studentDAO);
         };
+    }
+
+    private void deleteAllStudents(StudentDAO studentDAO) {
+        System.out.println("Deleting all students");
+       int numb = studentDAO.deleteAll();
+       System.out.println(numb);
+    }
+
+    private void deleteStudent(StudentDAO studentDAO) {
+        int studentId = 3;
+        System.out.println("Deleting student with id:" + studentId);
+        studentDAO.delete(studentId);
     }
 
     private void updateStudent(StudentDAO studentDAO) {
